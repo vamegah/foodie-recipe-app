5ebab75d5b95de19f9e13cc946728b99bdb24204
vamegah <makvim@yahoo.com>
2025-02-28 16:45:49 -0500

    Add dispatch details in favoritesSlice

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -1,15 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  favoriterecipes: [], // Updated to handle favorite articles
+    favoriterecipes: [], // Updated to handle favorite articles
 };
 
 const favoritesSlice = createSlice({
-  name: "favorites",
-  initialState,
-  reducers: {
-   
-  },
+    name: "favorites",
+    initialState,
+    reducers: {
+
+        reducers: {
+            toggleFavorite: (state, action) => {
+                const recipe = action.payload; // Expecting the entire article object
+                const existingIndex = state.favoriterecipes.findIndex(
+                    (item) => item.id === recipe.idFood // Assuming articles have an 'id' property
+                );
+                if (existingIndex >= 0) {
+                    state.favoriterecipes.splice(existingIndex, 1); // Remove from favorites
+                } else {
+                    state.favoriterecipes.push(recipe); // Add to favorites
+                }
+            },
+
+        },
+    },
 });
 
 export const { toggleFavorite } = favoritesSlice.actions;
