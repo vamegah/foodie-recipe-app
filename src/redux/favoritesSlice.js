8e4f9efde08808ebf031053e6f76b3ae765ee177
vamegah <makvim@yahoo.com>
2025-02-28 20:03:38 -0500

    Add tthe save recipe function to the RecipesFormScreen component

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -13,7 +13,7 @@ const favoritesSlice = createSlice({
             const existingIndex = state.favoriterecipes.findIndex(
                 (item) => item.idFood === recipe.idFood // Assuming articles have an 'id' property
             );
-            if (existingIndex !== -1) {
+            if (existingIndex >= 0) {
                 state.favoriterecipes.splice(existingIndex, 1); // Remove from favorites
             } else {
                 state.favoriterecipes.push(recipe); // Add to favorites
